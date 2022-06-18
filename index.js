63e450c6fa2a2297b8413352543ba48d4711548b
josephAttia <josephattia159@gmail.com>
2022-06-17 22:57:02 -0400

    madeAtimerNotworking

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require("C:\\Users\\josep\\Documents\\rPlaceClone\\rplaceclone.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://rplaceclone-default-rtdb.firebaseio.com"
+});
+
+var db = admin.database();
+var ref = db.ref("/");
+
+ref.once("value", function (snapshot) {
+  var data = snapshot.val();   //Data is in JSON format.
+});
+
+
 const express = require('express');
 const app = express();
 const http = require('http');
@@ -17,11 +34,19 @@ io.on('connection', (socket) => {
 
   socket.on('gridStatus', (grid) => {
     socket.broadcast.emit('newGrid', grid);
+    ref.set(grid);
   });
 
+  socket.on('checkPlacementAllowance', (data) => {
+    if(data.timeLeft == 0 || data.timeLeft < -1) {
+      socket.emit('allowPlacement', true);
+    }
+    else {
+      socket.emit('allowPlacement', false);
+    }
+  });
 });
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
-
